1f7d2704018bd63beab52df4cdcf2d2b8f66f74b
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-20 15:59:14 +0200

    fix Oauth

--- index.js
+++ index.js
@@ -1,106 +1,16 @@
-var _ = require('lodash');
-var ig = require('instagram-node').instagram();
+var _ = require('lodash'),
+    util = require('./util.js'),
+    instagram = require('instagram-node').instagram();
 
-var globalPickResult = {
-    media_count: 'media_count',
-    name: 'name'
-};
+var pickInputs = {
+        'tag': { key: 'tag', validate: { req: true } }
+    },
+    pickOutputs = {
+        'media_count': 'media_count',
+        'name': 'name'
+    };
 
 module.exports = {
-
-    /**
-     * Return pick result.
-     *
-     * @param output
-     * @param pickTemplate
-     * @returns {*}
-     */
-    pickResult: function (output, pickTemplate) {
-        var result = {};
-        // map template keys
-        _.map(_.keys(pickTemplate), function (templateKey) {
-
-            var oneTemplateObject = pickTemplate[templateKey];
-            var outputKeyValue = _.get(output, templateKey);
-
-            if (!outputKeyValue) {
-
-                return result;
-            }
-            // if template key is object - transform, else just save
-            if (_.isObject(oneTemplateObject)) {
-                // if data is array - map and transform, else once transform
-                if (_.isArray(outputKeyValue)) {
-
-                    result = this._mapPickArrays(outputKeyValue, oneTemplateObject);
-                } else {
-
-                    result[oneTemplateObject.key] = this.pickResult(outputKeyValue, oneTemplateObject.fields);
-                }
-            } else {
-
-                _.set(result, oneTemplateObject, outputKeyValue);
-            }
-        }, this);
-
-        return result;
-    },
-
-    /**
-     * System func for pickResult.
-     *
-     * @param mapValue
-     * @param templateObject
-     * @returns {*}
-     * @private
-     */
-    _mapPickArrays: function (mapValue, templateObject) {
-
-        var arrayResult = [],
-            result = templateObject.key? {} : [];
-
-        _.map(mapValue, function (inOutArrayValue) {
-
-            arrayResult.push(this.pickResult(inOutArrayValue, templateObject.fields));
-        }, this);
-
-        if (templateObject.key) {
-
-            result[templateObject.key] = arrayResult;
-        } else {
-
-            result = arrayResult;
-        }
-
-        return result;
-    },
-
-    /**
-     * Set acess token.
-     *
-     * @param dexter
-     */
-    authParams: function (dexter) {
-
-        if (dexter.environment('instagram_access_token')) {
-
-            ig.use({access_token: dexter.environment('instagram_access_token')});
-        } else {
-
-            this.fail('A [instagram_access_token] environment is Required.');
-        }
-    },
-
-    prepareStringInputs: function (inputs) {
-        var result = {};
-
-        _.map(inputs, function (inputValue, inputKey) {
-
-            result[inputKey] = _(inputValue).toString();
-        });
-
-        return result;
-    },
 
     /**
      * The main entry point for the Dexter module
@@ -109,25 +19,18 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var credentials = dexter.provider('instagram').credentials(),
+            inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
 
-        var tag = step.input('tag').first();
+        // check params.
+        if (validateErrors)
+            return this.fail(validateErrors);
 
-        if (!tag) {
+        instagram.use({ access_token: _.get(credentials, 'access_token') });
+        instagram.tag(inputs.tag, function (error, result) {
 
-            this.fail('A [tag] is Required.');
-        }
-
-        this.authParams(dexter);
-
-        ig.tag(tag, function (err, result) {
-
-            if (err) {
-
-                this.fail(err);
-            } else {
-
-                this.complete(this.pickResult(result, globalPickResult));
-            }
+            error? this.fail(error) : this.complete(util.pickOutputs(result, pickOutputs));
         }.bind(this));
     }
 };
